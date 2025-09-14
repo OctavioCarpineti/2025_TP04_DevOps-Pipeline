8cdba0215d85c13ecf3ea4a57f134905ce07962c
Octavio_Carpineti <2200631@ucc.edu.ar>
2025-09-14 16:18:18 -0300

    Mod
    Agrego app al repositorio para comenzar con TP4

---
+++ auth-system-devops/README.md
@@ -0,0 +1,2 @@
+# 2025_TP03_Auth-System-DevOps
+Authentication system for Azure DevOps learning - IngSW3
---
+++ auth-system-devops/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Auth System</h1>
+        <p>Welcome to the authentication system</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
